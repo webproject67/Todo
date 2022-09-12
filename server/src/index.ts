4c33d5ec0d7e38498aa0b0d2e3da79421f6078d0
Vladislav <webproject67@gmail.com>
2022-09-12 19:18:10 +0300

    установка helmet, compression

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import express, { Application } from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
 import bodyParser from 'body-parser';
 import multer from 'multer';
 import dbInit from './db/init';
@@ -14,6 +16,8 @@ const upload = multer();
 dbInit();
 
 app.use(cors());
+app.use(helmet());
+app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.none());
